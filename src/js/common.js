744cffe4dbdaff0abd5b0a127827704023b9844c
kotvasili <kot_vasili@mail.ru>
2016-10-14 17:00:32 +0300

    Desctop menu + dropdown

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 
-	// go js
+ function DesktopMenu(){
+	
+	var mainCont = $('.c-menu-block.main'),
+		items = mainCont.find('.js-desktop-hover'),
+		targetWrap = $('.menu-dropdown-wrapper'),
+		targetItems = targetWrap.find('.menu-dropdown-cont'),
+		shown = 'is-shown',
+		current = 'is-current';
+	items.each(function(){
+		var _ = $(this),
+			link = _.find('a'),
+			id = parseInt(link.data('elem'));
+			_.on('mouseenter',function(){
+				targetWrap.addClass(shown);
+				targetWrap.find("[data-elem="+ id +"]").addClass(current).siblings().removeClass(current);
+
+			})
+	});
+	mainCont.add(targetWrap).on('mouseleave',function(){
+		console.log($('.menu-dropdown-wrapper:hover').length)
+		if ($('.menu-dropdown-wrapper:hover').length != 1) {
+		    targetWrap.removeClass(shown);
+		}
+		
+	})
+	
+};
+DesktopMenu()
 
 });
